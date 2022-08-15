782a3eeba49f7fd9f2a52b64cb3bea24a0aa08d2
FaisalIsmail <faisalkhan7842540@gmail.com>
2022-08-15 23:05:57 +0500

    15-08-2022

--- pages/checkout.js
+++ pages/checkout.js
@@ -11,20 +11,20 @@ const Checkout = (props) => {
         <div className='px-2 w-1/2 '> 
       <div className=" mb-4">
         <label htmlFor="name" className=" font-bold  leading-7 text-md text-gray-600">Name</label>
-        <input type="text " id="name" name="name" className="font-bold w-full bg-white  rounded-lg border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+        <input type="text " id="name" name="name" className="font-bold w-full bg-white  rounded-lg border border-gray-300 focus:border-rose-500 focus:ring-2 focus:ring-rose-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
         </div>
       </div>
       <div className='px-2 w-1/2'> 
       <div className=" mb-4">
         <label htmlFor="email" className=" font-bold leading-7 text-md text-gray-600">Email</label>
-        <input type="email" id="email" name="email" className="font-bold  w-full bg-white rounded-lg border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+        <input type="email" id="email" name="email" className="font-bold  w-full bg-white rounded-lg border border-gray-300 focus:border-rose-500 focus:ring-2 focus:ring-rose-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
         </div>
       </div>
       </div>
       <div className='px-2 w-full'> 
       <div className=" mb-4">
         <label htmlFor="textarea" className="font-bold leading-7 text-md text-gray-600">Address</label>
-        <textarea className="font-bold  w-full bg-white rounded-lg border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" name="address" id="address" cols="30" rows="1"></textarea>
+        <textarea className="font-bold  w-full bg-white rounded-lg border border-gray-300 focus:border-rose-500 focus:ring-2 focus:ring-rose-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" name="address" id="address" cols="30" rows="1"></textarea>
         
       </div>
       </div>
@@ -32,13 +32,13 @@ const Checkout = (props) => {
         <div className='px-2 w-1/2 '> 
       <div className=" mb-4">
         <label htmlFor="phone" className=" font-bold  leading-7 text-md text-gray-600">Phone</label>
-        <input type="phone" id="phone" name="phone" className="font-bold w-full bg-white  rounded-lg border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+        <input type="phone" id="phone" name="phone" className="font-bold w-full bg-white  rounded-lg border border-gray-300 focus:border-rose-500 focus:ring-2 focus:ring-rose-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
         </div>
       </div>
       <div className='px-2 w-1/2'> 
       <div className=" mb-4">
         <label htmlFor="city" className=" font-bold leading-7 text-md text-gray-600">City</label>
-        <input type="text" id="city" name="city" className="font-bold  w-full bg-white rounded-lg border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+        <input type="text" id="city" name="city" className="font-bold  w-full bg-white rounded-lg border border-gray-300 focus:border-rose-500 focus:ring-2 focus:ring-rose-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
         </div>
       </div>
       </div>
@@ -46,13 +46,13 @@ const Checkout = (props) => {
         <div className='px-2 w-1/2 '> 
       <div className=" mb-4">
         <label htmlFor="state" className=" font-bold  leading-7 text-md text-gray-600">State</label>
-        <input type="text" id="state" name="state" className="font-bold w-full bg-white  rounded-lg border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+        <input type="text" id="state" name="state" className="font-bold w-full bg-white  rounded-lg border border-gray-300 focus:border-rose-500 focus:ring-2 focus:ring-rose-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
         </div>
       </div>
       <div className='px-2 w-1/2'> 
       <div className=" mb-4">
         <label htmlFor="pincode" className=" font-bold leading-7 text-md text-gray-600">Pincode</label>
-        <input type="text" id="pincode" name="pincode" className="font-bold  w-full bg-white rounded-lg border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+        <input type="text" id="pincode" name="pincode" className="font-bold  w-full bg-white rounded-lg border border-gray-300 focus:border-rose-500 focus:ring-2 focus:ring-rose-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
         </div>
       </div>
       </div>
